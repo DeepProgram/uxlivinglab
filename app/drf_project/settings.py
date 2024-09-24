887290499944b644d8222985d5972376eb9087f5
Deep Sarker <deepsarker01@gmail.com>
2024-09-24 13:57:30 +0600

    add dotenv to load env files

--- app/drf_project/settings.py
+++ app/drf_project/settings.py
@@ -12,12 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from sqlite3 import connect
 import mongoengine
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv.read_dotenv(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
